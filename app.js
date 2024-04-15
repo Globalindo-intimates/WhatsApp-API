7aa0152d9b3c83fdae166289632996a00fe24bfd
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2024-04-15 11:02:52 +0700

    Update: AuthStrategy

--- app.js
+++ app.js
@@ -43,8 +43,13 @@ const client = new Client({
     ],
   },
   authStrategy: new LocalAuth({
-    clientId: "Adrian",
+    dataPath: "sessions",
   }),
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
 });
 
 client.initialize();
